dfd0d432bf9783dd24182966327754a8be10784c
siyeon <hansi0121@naver.com>
2021-06-23 01:38:16 +0900

    commit

--- algorithm/Programmers/Programmers_Solution02.py
+++ algorithm/Programmers/Programmers_Solution02.py
@@ -23,7 +23,7 @@
 def solution(n, money):
     # dp생성
     dp = [[0] * (n + 1) for _ in range(len(money))]
-    print(dp)
+
     # 0원은 지불 가능하니까 1로 시작
     dp[0][0] = 1
 
@@ -35,7 +35,6 @@ def solution(n, money):
     for y in range(1, len(money)):
         # x: 거슬러줘야하는 금액
         for x in range(n + 1):
-            print(x)
             # 거슬러줘야하는 금액을 지불가능한 동전보다 클 경우
             if x >= money[y]:
                 dp[y][x] = (dp[y - 1][x] + dp[y][x - money[y]]) % 1000000007
