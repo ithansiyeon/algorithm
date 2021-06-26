8dd9a53854840fb69506a510c6f08c2f07991b35
siyeon <hansi0121@naver.com>
2021-06-26 18:11:03 +0900

    commit

---
+++ algorithm/Baejoon/Baejoon_Solution16.py
@@ -0,0 +1,13 @@
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+#
+# 입력
+# 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+#
+# 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+N = int(input())
+nums = []
+for i in range(N):
+    nums.append(input().split(" "))
+
+for num in nums:
+    print(int(num[0])+int(num[1]))
