ed6d3a4b09ef0d23a8bc0b51c4161a023bd246e3
siyeon <ithansiyeon@nate.com>
2022-04-10 08:34:27 -0700

    commit

--- algorithm/test/sorting/성적이 낮은 순서로 학생 출력하기.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-data = []
-for i in range(n):
-    data.append(input().split(" "))
-
-array = sorted(data,key=lambda data: data[1])
-
-for student in array:
-    print(student[0],end=' ')
