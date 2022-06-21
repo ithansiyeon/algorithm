164d66cba1ccd42c5d56f726a20fcbcdeb0ee1da
ithansiyeon <itsiyeon@nate.com>
2022-06-21 23:35:07 +0900

    그리디 알고리즘 - 큰 수의 법칙

--- algorithm/coding/큰 수의 법칙.py
+++ algorithm/coding/큰 수의 법칙.py
@@ -1,18 +1,12 @@
 n,m,k = map(int,input().split())
 nlist = list(map(int,input().split()))
 nlist.sort(reverse=True)
-cnt = 0
-res = 0
 
-while cnt != m:
-    for i in range(k):
-        if cnt == m:
-            break
-        res+=nlist[0]
-        cnt+=1
-    if cnt == m:
-        break
-    res+=nlist[1]
-    cnt+=1
+count = int(m//(k+1))*k
+count += m%(k+1)
+
+res = 0
+res += count*nlist[0]
+res += (m-count)*nlist[1]
 
 print(res)
